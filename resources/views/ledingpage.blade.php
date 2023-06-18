9efff72351be99060eaeb1c9c3bf08ceb766952e
unknown <zainiilham582@gmail.com>
2023-06-18 03:34:16 -1200

    k2 comodo

---
+++ resources/views/ledingpage.blade.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>komodo</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/download.png" rel="icon">
+  <link href="assets/img/download.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Arsha
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">KOMODO</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <ul class="navbar-nav ms-auto">
+        <!-- Authentication Links -->
+
+    </ul>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>ODEYO</h1>
+          <h2>MANAGEMENT APPLICATION<h2>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            @guest
+            @if (Route::has('login'))
+                <li class="nav-item">
+                    <a class="btn-get-started scrollto" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+            @endif
+            </div>
+                <div class="d-flex justify-content-center justify-content-lg-start">
+                    @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="btn-get-started scrollto" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+                </div>
+
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-end" >
+                    <a class="dropdown-item" href="#"
+                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients section-bg">
+      <div class="container">
+
+        <div class="row" data-aos="zoom-in">
+
+
+
+        </div>
+
+      </div>
+    </section><!-- End Cliens Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Team</h2>
+          <p>KELOMPOK 2</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-1.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Ady Putra Ramadhan</h4>
+                <span>Ketua Kelompok KOMODO</span>
+                <p>IST AKPRIND YOGYAKARTA</p>
+                <div class="social">
+                  <a href="https://www.twitter.com/"><i class="ri-twitter-fill"></i></a>
+                  <a href="https://www.facebook.com/"><i class="ri-facebook-fill"></i></a>
+                  <a href="https://www.instagram.com/adypr_____11"><i class="ri-instagram-fill"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-2.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Safna Aulia Salsabila</h4>
+                <span>Anggota Kelompok KOMODO</span>
+                <p>Universitas Putra Bangsa KEBUMEN</p>
+                <div class="social">
+                  <a href="https://www.twitter.com/"><i class="ri-twitter-fill"></i></a>
+                  <a href="https://www.facebook.com/"><i class="ri-facebook-fill"></i></a>
+                  <a href="https://www.instagram.com/sfnauliasal_"><i class="ri-instagram-fill"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="300">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>M. Zaini Ilham</h4>
+                <span>Anggota Kelompok KOMODO</span>
+                <p>STMIK SAYIKH ZAINUDDIN LOMBOK TIMUR</p>
+                <div class="social">
+                  <a href="https://www.twitter.com/"><i class="ri-twitter-fill"></i></a>
+                  <a href="https://www.facebook.com/"><i class="ri-facebook-fill"></i></a>
+                  <a href="https://www.instagram.com/zainiilham_"><i class="ri-instagram-fill"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-5.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Lukman Yahya Saputra</h4>
+                <span>Anggota Kelompok KOMODO</span>
+                <p>Universitas AMIKOM PURWOKERTO</p>
+                <div class="social">
+                  <a href="https://www.twitter.com/"><i class="ri-twitter-fill"></i></a>
+                  <a href="https://www.facebook.com/"><i class="ri-facebook-fill"></i></a>
+                  <a href="https://www.instagram.com/"><i class="ri-instagram-fill"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/team-4.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Heru Setiawan Saputra</h4>
+                <span>Anggota Kelompok KOMODO</span>
+                <p>Universitas PGRI SEMARANG</p>
+                <div class="social">
+                  <a href="https://www.twitter.com/"><i class="ri-twitter-fill"></i></a>
+                  <a href="https://www.facebook.com/"><i class="ri-facebook-fill"></i></a>
+                  <a href="https://www.instagram.com/"><i class="ri-instagram-fill"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+    <!--
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+       KELOMPOK 2 <a href="https://bootstrapmade.com/">KOMODO</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
